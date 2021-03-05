ba36b4cd1bc5e841e736a93877e7c3cff4294757
steve <steve@DESKTOP-5VM4L8A.mshome.net>
2021-03-05 13:31:57 -0500

    First commit

---
+++ testGit/src/testGit/TestGit.java
@@ -0,0 +1,11 @@
+package testGit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A git test java project. Main called.");
+
+	}
+
+}
